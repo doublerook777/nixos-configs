f1d1c0b204f015c94a9b0448a04f68a05a6a46b3
caelum <sjgupta30@gmail.com>
2026-06-17 12:36:37 +0530

    making-loupe-default

--- home.nix
+++ home.nix
@@ -65,6 +65,18 @@
       categories = [ "Utility" "Core" "System" ];
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+     "image/png" = "org.gnome.Loupe.desktop";
+     "image/jpeg" = "org.gnome.Loupe.desktop";
+     "image/gif" = "org.gnome.Loupe.desktop";
+     "image/webp" = "org.gnome.Loupe.desktop";
+     "image/bmp" = "org.gnome.Loupe.desktop";
+     "image/tiff" = "org.gnome.Loupe.desktop";
+    };
+  };
+
   programs.fuzzel = {
     enable = true;
     settings = {
